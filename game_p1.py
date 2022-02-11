fef6c9b320dc657c49894a903e48ed1cf7e20b1b
IagoGarciaSuarez <iago.garcia.suarez.ds@gmail.com>
2022-02-11 20:22:33 +0100

    Put tile on clic
    Added possibility to draw a tile when clic on a position, centering the image to that cell

--- game_p1.py
+++ game_p1.py
@@ -1,4 +1,3 @@
-from ast import unparse
 import pygame
 from pygame.locals import (
     K_ESCAPE,
@@ -6,7 +5,7 @@ from pygame.locals import (
     QUIT,
 )
 import const
-from utils import parse_coords
+from utils import parse_coords, scale_img, unparse_coords
 
 def main():
     pygame.init()
@@ -18,16 +17,20 @@ def main():
     clock = pygame.time.Clock()
 
     window = pygame.display.set_mode((const.WIDTH, const.HEIGHT))
+    screen = pygame.display.get_surface()
     pygame.display.set_caption('Juego del Molino')
     pygame.display.set_icon(pygame.image.load(const.LOGO))
 
     bg_img = pygame.image.load(const.BOARD)
     bg_img = pygame.transform.scale(bg_img,(const.WIDTH, const.HEIGHT))
+    map_tiles = []
 
+    turn = 1
     running = True
     while running:
-        clock.tick(15)
         window.blit(bg_img, (0,0))
+        window.blits(map_tiles)
+        clock.tick(15)
         pygame.draw.line(window, (255, 0, 0), (25, 25), (25, const.HEIGHT-25))
         pygame.draw.line(window, (255, 0, 0), (25, const.HEIGHT-25), (const.WIDTH-25, const.HEIGHT-25))
         pygame.draw.line(window, (255, 0, 0), (25, 25), (const.WIDTH - 25, 25))
@@ -37,16 +40,28 @@ def main():
             pygame.draw.line(window, (255, 0, 0), (25 + const.BLOCKSIZE*i, 0), (const.BLOCKSIZE*i+25, const.HEIGHT))
             pygame.draw.line(window, (255, 0, 0), (0, 25 + const.BLOCKSIZE*i), (const.HEIGHT, const.BLOCKSIZE*i+25))
 
+        molino_j1_img = scale_img("assets/image/molino_j1.png", (const.BLOCKSIZE - 10, const.BLOCKSIZE - 10))
+        molino_j2_img = scale_img("assets/image/molino_j2.png", (const.BLOCKSIZE - 10, const.BLOCKSIZE - 10))
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 if not (pos[0] < 25 or pos[1] < 25 or pos[0] > const.HEIGHT - 25 or pos[1] > const.WIDTH - 25):
-                    print(parse_coords(pos))
+                    pcords = parse_coords(pos)
+                    print(pcords)
+                    if turn == 1:
+                        rect = molino_j1_img.get_rect(center = unparse_coords(pcords))
+                        map_tiles.append((molino_j1_img, rect))
+                    if turn == -1:
+                        rect = molino_j2_img.get_rect(center = unparse_coords(pcords))
+                        map_tiles.append((molino_j2_img, rect))
+                    turn *= -1
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 running = False
             elif event.type == QUIT:
                 running = False
+
         pygame.display.update()
+
     pygame.quit()
 
 
